d6e0d6a413e4be9ca684085e7a828916e9243796
Moureen774 <moureen774@gmail.com>
2025-03-10 19:35:07 +0300

    Add files via upload

---
+++ for loop 1.cpp
@@ -0,0 +1,18 @@
+/*
+For loop
+Author:Moureen Ndinda
+Reg no:D33-5024-2023
+Date:10-3-2025
+*/
+
+#include <stdio.h>
+
+int main(){
+	int i;
+	for(i=1; i<=5; i++) {
+		printf("%d \n", i*i*i);
+	}
+	
+	return 0; // successful execution
+	
+}
