e81adc680e97ea2606ea79257a30a0f7c1804ff7
Moureen774 <moureen774@gmail.com>
2025-03-10 19:37:09 +0300

    Add files via upload

---
+++ Do-while loop 1.cpp
@@ -0,0 +1,22 @@
+/*
+do-while loops
+Author:Moureen Ndinda
+Reg no:D33-5024-2023
+Date:10-3-2025
+
+*/
+#include <stdio.h>
+
+int main() {
+	int i=1; // start 
+	do{
+	printf("%d \n", i*i*i);
+	i++; 
+	
+	
+	
+	}while(i<=5);
+	
+	return 0; // successful execution
+}
+	
